f61f14003e516c27773b058995ade17194de53bb
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2022-01-26 22:57:14 +0530

    Time: 8 ms (7.26%), Space: 7.3 MB (58.46%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        // support variables
+        int res = 0, seen[101] = {};
+        // populating seen with all the frequencies
+        for (int n: nums) seen[n]++;
+        // updating res based on the found frequencies
+        for (int f: seen) if (f) res += f * (f - 1) / 2;
+        return res;
+    }
+};
