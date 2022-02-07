d4c8611f0a6eb68f9c6efebd0c44c27e94f715f4
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2022-02-07 21:50:55 +0530

    Time: 24 ms (69.66%), Space: 23.2 MB (41.45%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -4,7 +4,7 @@ public:
         int start = 0;
         int end = s.size()-1;
         
-        while(start<end){
+        while(start<=end){
             swap(s[start++], s[end--]);
         }
     }
