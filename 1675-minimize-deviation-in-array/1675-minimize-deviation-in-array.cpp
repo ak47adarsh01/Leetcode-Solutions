33307369195787a095e6c3d0217d149a62afecff
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2022-02-19 15:34:53 +0530

    Time: 289 ms (56.95%), Space: 71.3 MB (83.44%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,56 +1,27 @@
 class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
-        int n = nums.size();
-        // pretreatment
-        vector<int> possibleList[n];
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 == 0) {
-                int temp = nums[i];
-                possibleList[i].push_back(temp);
-                while (temp % 2 == 0) {
-                    temp /= 2;
-                    possibleList[i].push_back(temp);
-                }
+        int x = INT_MAX, res;
+        priority_queue<int> pq;
+        
+        for (auto& n:nums) {
+            if (n&1) {
+                pq.push(n*2);
+                x = min(x, n*2);
             } else {
-                possibleList[i].push_back(nums[i] * 2);
-                possibleList[i].push_back(nums[i]);
-            }
-            // reverse candidates to sort from small to large
-            reverse(possibleList[i].begin(), possibleList[i].end());
-        }
-        vector<vector<int>> pointers;
-        for (int i = 0; i < n; i++) {
-            int size = possibleList[i].size();
-            for (int j = 0; j < size; j++) {
-                vector<int> initialPointer{possibleList[i][j], i, j};
-                pointers.emplace_back(move(initialPointer));
+                pq.push(n);
+                x = min(x, n);
             }
         }
-        sort(pointers.begin(), pointers.end());
-        int minDeviation = INT_MAX;
-        int currentEnd = 0;
-        for (int i = 0; i < n; i++) {
-            currentEnd = max(currentEnd, possibleList[i][0]);
+        
+        res = pq.top() - x;
+        while (!(pq.top()&1)) {
+            int temp  = pq.top()/2; pq.pop();
+            x = min(x, temp);
+            pq.push(temp);
+            res = min(res, pq.top() - x);
         }
-        int possibleListSize[n];
-        for (int i = 0; i < n; i++) {
-            possibleListSize[i] = possibleList[i].size();
-        }
-        for (auto& current : pointers) {
-            int currentStart = current[0];
-            int index = current[1];
-            int indexInCandidates = current[2];
-            if (minDeviation > currentEnd - currentStart) {
-                minDeviation = currentEnd - currentStart;
-            }
-            // if the pointer reach last
-            if (indexInCandidates + 1 == possibleListSize[index]) {
-                return minDeviation;
-            }
-            int nextValue = possibleList[index][indexInCandidates + 1];
-            currentEnd = max(currentEnd, nextValue);
-        }
-        return 1;
+        
+        return res;
     }
 };
