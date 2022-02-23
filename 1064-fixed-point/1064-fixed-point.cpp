e78abb052d4813509a52c34e5eb3285023cc3fa1
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2022-02-23 11:20:51 +0530

    Time: 12 ms (68.56%), Space: 10.6 MB (13.40%) - LeetHub

--- 1064-fixed-point/1064-fixed-point.cpp
+++ 1064-fixed-point/1064-fixed-point.cpp
@@ -8,7 +8,7 @@ public:
         int ans = -1;
         
         while( start <= end){
-            int mid =  ( start + end) /2;
+            int mid =  start + ( end - start) /2;
             if(arr[mid] == mid){
             
                 ans  = mid;
