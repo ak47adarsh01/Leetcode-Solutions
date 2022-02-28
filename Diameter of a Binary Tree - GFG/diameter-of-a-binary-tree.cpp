36b61d8703208d2889ad75dde4d79abb6b1904b9
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2022-02-28 14:49:13 +0530

    Added solution - LeetHub

--- Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -93,21 +93,31 @@ struct Node
 
 class Solution {
  public:
-  int diam=-1;
-  int find(Node *root){
-      if(root==NULL)
-      return 0;
-      int lh=find(root->left);
-      int rh=find(root->right);
-      if(lh+rh+1>diam)
-      diam=lh+rh+1;
-      return max(lh+ 1,rh+1);
-  }
-   int diameter(Node* root) {
-   find(root);
-   return diam;
-     
-   }
+    pair<int,int> diameterFast(Node* root) {
+        //base case
+        if(root == NULL) {
+            pair<int,int> p = make_pair(0,0);
+            return p;
+        }
+        
+        pair<int,int> left = diameterFast(root->left);
+        pair<int,int> right = diameterFast(root->right);
+        
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second + 1;
+        
+        pair<int,int> ans;
+        ans.first = max(op1, max(op2, op3));;
+        ans.second = max(left.second , right.second) + 1;
+
+        return ans;
+    }
+    int diameter(Node* root) {
+    
+        return diameterFast(root).first;
+        
+    }
 };
 
 
