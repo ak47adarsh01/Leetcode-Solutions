814b6e9dd445664023f9653a2e3bdac3599bc9c0
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2022-03-03 14:01:14 +0530

    Time: 17 ms (53.23%), Space: 11.9 MB (50.00%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        auto comp = [](string &a, string &b) {
+            string aa = a.substr(a.find(' '));
+            string bb = b.substr(b.find(' '));
+            
+            return aa == bb ? a < b : aa < bb;
+        };
+        
+        vector<string> letters;
+        vector<string> digits;
+        for (string &str: logs) {
+            int i = 0;
+            while (i < str.size() && str[i++] != ' ');
+            if (isalpha(str[i]))
+                letters.push_back(str);
+            else
+                digits.push_back(str);
+        }
+        
+        sort(letters.begin(), letters.end(), comp);
+        
+        letters.insert(letters.end(), digits.begin(), digits.end());
+        return letters;
+    }
+};
