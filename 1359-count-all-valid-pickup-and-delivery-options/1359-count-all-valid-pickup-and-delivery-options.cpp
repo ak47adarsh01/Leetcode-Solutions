f4b62f1896666657bfdcdb8619694c46b6235ba3
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2022-03-06 23:54:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.14%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -3,15 +3,17 @@ public:
     int countOrders(int n) {
         long ans = 1;
         int MOD = 1e9 + 7;
-
-        for (int i = 1; i <= n; ++i) {
-            // Ways to arrange all pickups, 1*2*3*4*5*...*n
+        
+        for (int i = 1; i <= 2 * n; ++i) {
             ans = ans * i;
-            // Ways to arrange all deliveries, 1*3*5*...*(2n-1)
-            ans = ans * (2 * i - 1);
+            
+            // We only need to divide the result by 2 n-times.
+            // To prevent decimal results we divide after multiplying an even number.
+            if (i % 2 == 0) {
+                ans = ans / 2;
+            }
             ans %= MOD;
         }
-        
         return ans;
     }
 };
