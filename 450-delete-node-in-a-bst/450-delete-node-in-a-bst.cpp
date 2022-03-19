69bfbf4292a173f8cb1c45258ed0ce45056644c6
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2022-03-19 10:57:50 +0530

    Time: 63 ms (21.75%), Space: 32.8 MB (31.63%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -11,21 +11,35 @@
  */
 class Solution {
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root) 
-            if(key < root->val) root->left = deleteNode(root->left, key);     
-            else if(key > root->val) root->right = deleteNode(root->right, key);       
-            else{
-                if(!root->left && !root->right) return NULL;          
-                if (!root->left || !root->right)
-                    return root->left ? root->left : root->right;    
-					                                                  
-                TreeNode* temp = root->left;                        
-                while(temp->right != NULL) temp = temp->right;     
-                root->val = temp->val;                            
-                root->left = deleteNode(root->left, temp->val);         
-                		
+    TreeNode* deleteNode(TreeNode* root, int val) {
+        if (!root) return root;
+        
+        if (val < root->val) {
+            root->left = deleteNode(root->left, val);
+        } else if (val > root->val) {
+            root->right = deleteNode(root->right, val);
+        } else {
+            /* Leaf node case */
+            if (!root->left && !root->right) {
+                delete(root);
+                return NULL;
             }
+            /* 1 child case */
+            if (!root->left || !root->right) {
+                TreeNode *ret = root->left ? root->left : root->right;
+                delete(root);
+                return ret;
+            }
+            /* 2 child case */
+            if (root->left && root->right) {
+                TreeNode *tmp = root->right;
+                while (tmp->left) {
+                    tmp = tmp->left;
+                }
+                root->val = tmp->val;
+                root->right = deleteNode(root->right, root->val);
+            }
+        }
         return root;
-    }   
+    }
 };
