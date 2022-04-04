62392faa858b151489fe251ae6e922e499e96463
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2022-04-04 20:18:15 +0530

    Time: 923 ms (43.24%), Space: 180.1 MB (89.18%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,26 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* swapNodes(ListNode* head, int k) {
-        int listLength = 0;
-        ListNode* frontNode = nullptr;
-        ListNode* endNode = nullptr;
-        ListNode* currentNode = head;
-        // set the front node and end node in single pass
-        while (currentNode) {
-            listLength++;
-             if(endNode != NULL) {
-                endNode = endNode->next;
-             }
-            // check if we have reached kth node
-            if (listLength == k) {
-                frontNode = currentNode;
-                endNode = head;
-            }          
-            currentNode = currentNode->next;
+    int length(ListNode* head) { // to find the length of linked list
+        int count = 0;
+        while (head){
+            count++;
+            head = head->next;
         }
-        // swap the values of front node and end node
-        swap(frontNode->val, endNode->val);
+        return count;
+    }
+    
+    ListNode* swapNodes(ListNode* head, int k) {
+        
+        int val1, val2, i=0, sz = length(head);
+        
+        if (sz == 1)
+            return head;
+        
+        k--; // for making it 1 based indexing to 0 based indexing (for easy understanding)
+        
+        if (k==(sz/2) and sz%2) // if its exactly middle element in the list, then return head coz no need of swapping
+            return head;
+         
+        ListNode* curr = head;
+        ListNode *cur1, *cur2;
+        
+        while(curr){
+            if (i==k) {  // 1st node to swap
+                val1 = curr->val;
+                cur1 = curr;
+            }
+            else if (i==(sz-k-1)){ // 2nd node to swap
+                val2 = curr->val;
+                cur2 = curr;
+            }
+            
+            curr = curr->next;
+            i++;
+        }
+        
+        cur1->val = val2; // exchanging the values with each other 
+        cur2->val = val1;
+        
         return head;
     }
 };
